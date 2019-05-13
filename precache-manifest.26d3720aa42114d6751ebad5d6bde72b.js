144a90e9b3557b830ac16d1c50dd4b7e06706614
zhenghao0723 <50564948+zhenghao0723@users.noreply.github.com>
2019-05-13 13:26:51 +0800

    Updates

---
+++ precache-manifest.26d3720aa42114d6751ebad5d6bde72b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e8ebded4d15d08a991218b936c75e55",
+    "url": "/digital-signage/index.html"
+  },
+  {
+    "revision": "052ce29a6a24ddd62f8b",
+    "url": "/digital-signage/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/digital-signage/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "052ce29a6a24ddd62f8b",
+    "url": "/digital-signage/static/js/main.4ad77c4d.chunk.js"
+  },
+  {
+    "revision": "ec0c101fde480160623d",
+    "url": "/digital-signage/static/js/runtime~main.8a86d63a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/digital-signage/static/media/logo.5d5d9eef.svg"
+  }
+]);
